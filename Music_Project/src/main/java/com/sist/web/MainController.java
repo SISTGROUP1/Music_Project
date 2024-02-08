803313248dd3624102d1c479c1327468aceeb38f
user <user@DESKTOP-SLJMTH5>
2024-02-08 14:39:03 +0900

    신아라-템플릿 적용

--- Music_Project/src/main/java/com/sist/web/MainController.java
+++ Music_Project/src/main/java/com/sist/web/MainController.java
@@ -1,7 +1,12 @@
 package com.sist.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class MainController {
-	public static void main(String[] args) {
-		System.out.println("안녕하세요.");
+	@GetMapping("main/main.do")
+	public String main_main()
+	{
+		return "main";
 	}
 }
